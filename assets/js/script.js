b86bd2faf0c97b094542cf9c9f2d3cac7ada8b56
Elizabeth Wu <elizawu@berkeley.edu>
2020-12-01 18:06:48 -0800

    fixed responsiveness js tooltip not working -_-

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,8 @@
-function unhideLightbox(lightboxID) {
-	// SHOW overlay
-	document.getElementById('lightbox-overlay').classList.remove('hidden');
-	// SHOW lightbox image
-	document.getElementById(lightboxID).classList.remove('hidden');
+function imagePopup() {
+	let dog = document.getElementById("dog");
+	dog.classList.remove("hidden");
 }
 
-function unhideLightboxes() {
-	unhideLightbox(slidename);
-}
+let imgText = document.getElementById("dogimg");
 
-let len = document.getElementsByClassName('lightbox').length;
-
-for (let i = 1; i <= len; i++) {
-	let slidename = "slide" + i;
-	document.getElementById(slidename).onclick = unhideLightboxes;
-}
-
-//TODO: add media response query / webpage size responsiveness
-//animate background circles
-//hover over intro text for pictures w/ css? or potentially JS
+imgText.onmouseover = imagePopup;
